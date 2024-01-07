411012072a8a46c2dc3cbb8fcdd35a5a2426238c
furkanackgz <n.furkan.acikgoz@gmail.com>
2024-01-07 11:04:02 +0300

    Text

--- SwiftfullThinkingBootcamp/ContentView.swift
+++ SwiftfullThinkingBootcamp/ContentView.swift
@@ -10,10 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, World! This is Furkan, an iOS Developer from Arcelik Global!")
+//                .font(.title)
+//                .fontWeight(.semibold)
+                .foregroundStyle(Color.green)
+//                .underline(true, color: .mint)
+//                .strikethrough(true, color: .orange)
+//                .italic()
+//                .padding()
+                .kerning(2.0)
+                .baselineOffset(20)
+                .multilineTextAlignment(.leading)
+                .font(.system(size: 21,
+                              weight: .medium,
+                              design: .serif)
+                )
+//                .frame(width: 100, height: 100, alignment: .trailing)
+                .minimumScaleFactor(0.3)
         }
         .padding()
     }
