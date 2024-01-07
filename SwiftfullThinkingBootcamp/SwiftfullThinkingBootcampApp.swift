6f718cded9411ce351fcc1799743f50b0013b0a4
furkanackgz <n.furkan.acikgoz@gmail.com>
2024-01-07 10:17:50 +0300

    Initial Commit

---
+++ SwiftfullThinkingBootcamp/SwiftfullThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfullThinkingBootcampApp.swift
+//  SwiftfullThinkingBootcamp
+//
+//  Created by Furkan Açıkgöz on 7.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfullThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
