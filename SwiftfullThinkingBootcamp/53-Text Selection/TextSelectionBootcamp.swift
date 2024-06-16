7042228602cb81d0cf88b0e5326511ebbd3dd4f1
Nadi Furkan Açıkgöz <n.furkan.acikgoz@gmail.com>
2024-06-16 17:06:46 +0300

    Text Selection

---
+++ SwiftfullThinkingBootcamp/53-Text Selection/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftfullThinkingBootcamp
+//
+//  Created by Furkan Açıkgöz on 16.06.2024.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
