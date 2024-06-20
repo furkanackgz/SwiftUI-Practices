5754112c54f19edcfda618db79ffe2d4802291c5
Nadi Furkan Açıkgöz <n.furkan.acikgoz@gmail.com>
2024-06-20 15:55:03 +0300

    Menu

---
+++ SwiftfullThinkingBootcamp/64-Menu/MenuBootcamp.swift
@@ -0,0 +1,30 @@
+//
+//  MenuBootcamp.swift
+//  SwiftfullThinkingBootcamp
+//
+//  Created by Furkan Açıkgöz on 20.06.2024.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Click me") {
+            Button("Button 1") {
+                
+            }
+            
+            Button("Button 2") {
+                
+            }
+            
+            Button("Button 3") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuBootcamp()
+}
