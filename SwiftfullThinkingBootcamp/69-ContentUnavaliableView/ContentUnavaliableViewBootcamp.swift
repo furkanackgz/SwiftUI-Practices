d0240e86989e6715463ccc53f9f573405a178914
Nadi Furkan Açıkgöz <n.furkan.acikgoz@gmail.com>
2024-07-13 14:32:29 +0300

    ContentUnavaliableView

---
+++ SwiftfullThinkingBootcamp/69-ContentUnavaliableView/ContentUnavaliableViewBootcamp.swift
@@ -0,0 +1,24 @@
+//
+//  ContentUnavaliableViewBootcamp.swift
+//  SwiftfullThinkingBootcamp
+//
+//  Created by Furkan Açıkgöz on 13.07.2024.
+//
+
+import SwiftUI
+
+struct ContentUnavaliableViewBootcamp: View {
+    var body: some View {
+//        ContentUnavailableView(
+//            "No Internet",
+//            systemImage: "wifi.slash",
+//            description: Text("Please check your internet connection and try again.")
+//        )
+        
+        ContentUnavailableView.search
+    }
+}
+
+#Preview {
+    ContentUnavaliableViewBootcamp()
+}
